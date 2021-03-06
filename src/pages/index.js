2ead046273432f8275a648776b95a46f704228d4
Juraj Ligocky <j.ligocky@gmail.com>
2021-03-06 23:23:50 +0100

    Update index.js with data from contact json

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+import contact from "../../site/content/contact.json";
+
 // styles
 const pageStyles = {
   color: "#232129",
@@ -9,7 +11,7 @@ const pageStyles = {
 
 // markup
 const IndexPage = () => {
-  return <main style={pageStyles}>Cukon netlify CMS</main>;
+  return <main style={pageStyles}>Cukon {contact.phone}</main>;
 };
 
 export default IndexPage;
